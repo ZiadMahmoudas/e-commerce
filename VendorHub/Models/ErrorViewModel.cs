178e0b0c993e0d67d4e5a26e0b6ce9896437e194
Ziad Elmohager <ziadbobo78@gmail.com>
2026-03-11 03:21:45 +0200

    Add project files.

---
+++ VendorHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendorHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
