ac26bdbf1e7d22dcdf119dfc747ced39e4d10d99
Ziad Elmohager <ziadbobo78@gmail.com>
2026-03-11 13:03:52 +0200

    Change To React + WebAPi

--- VendorHub/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VendorHub.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
